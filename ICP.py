c7a6872ecf884b5cacfd53cfb8242354540fa6dc
YuhangMing <mingyuhang@gmail.com>
2021-02-16 11:35:41 +0000

    remove icp module in python

--- ICP.py
+++ ICP.py
@@ -8,8 +8,8 @@ Written by Srinath Sridhar
 from open3d import *
 import numpy as np
 import copy
-# from matplotlib.mlab import PCA
-from sklearn.decomposition import PCA
+# # from matplotlib.mlab import PCA
+# from sklearn.decomposition import PCA
 import random
 
 def draw_registration_result(source, target, transformation):
@@ -21,65 +21,68 @@ def draw_registration_result(source, target, transformation):
     draw_geometries([source_temp, target_temp])
 
 def doICP(model_pts_np, pred_pts_np, SigmaFactor=5, threshold=1000, isViz=False):
-    model_pts = PointCloud()
-    model_pts.points = Vector3dVector(model_pts_np)
-    model_pts_orig = model_pts
-    model_pts_orig_np = np.asarray(model_pts_orig.points)
-    pred_pts = PointCloud()
-    pred_pts.points = Vector3dVector(pred_pts_np)
+    # model_pts = PointCloud()
+    # model_pts.points = Vector3dVector(model_pts_np)
+    # model_pts_orig = model_pts
+    # model_pts_orig_np = np.asarray(model_pts_orig.points)
+    # pred_pts = PointCloud()
+    # pred_pts.points = Vector3dVector(pred_pts_np)
 
-    # First use PCA to compute the dimensions of target
-    print(model_pts_np.shape())
-    model_pca = PCA(model_pts_np)
-    print(pred_pts_np.shape())
-    pred_pca = PCA(pred_pts_np)
-    # print('Model mean:', model_pca.mu)
-    # print('Model sigma:', model_pca.sigma)
-    # print('Prediction mean:', pred_pca.mu)
-    # print('Prediction sigma:', pred_pca.sigma)
-    #
-    # Scale model and move its center to prediction center
-    ScaleFactor = np.array([1, 1, 1])
+    # # First use PCA to compute the dimensions of target
+    # print(model_pts_np.shape())
+    # model_pca = PCA(model_pts_np)
+    # print(pred_pts_np.shape())
+    # pred_pca = PCA(pred_pts_np)
+    # # print('Model mean:', model_pca.mu)
+    # # print('Model sigma:', model_pca.sigma)
+    # # print('Prediction mean:', pred_pca.mu)
+    # # print('Prediction sigma:', pred_pca.sigma)
+    # #
+    # # Scale model and move its center to prediction center
+    # ScaleFactor = np.array([1, 1, 1])
  
 
-    #ScaleFactor = pred_pca.sigma * SigmaFactor * SigmaFactor # OUTPUT 1
-    PredCenter = pred_pca.mu
-    for i in range(0, 3):
-        # model_pts_np[:, i] = (model_pts_np[:, i] - model_pca.mu[i]) * ScaleFactor[i] * Factor + pred_pca.mu[i]
-        model_pts_np[:, i] = (model_pts_np[:, i]) * ScaleFactor[i] + PredCenter[i] # Scale and then move model origin to origin of prediction depth point cloud
-        model_pts_orig_np[:, i] = (model_pts_orig_np[:, i]) * ScaleFactor[i]
+    # #ScaleFactor = pred_pca.sigma * SigmaFactor * SigmaFactor # OUTPUT 1
+    # PredCenter = pred_pca.mu
+    # for i in range(0, 3):
+    #     # model_pts_np[:, i] = (model_pts_np[:, i] - model_pca.mu[i]) * ScaleFactor[i] * Factor + pred_pca.mu[i]
+    #     model_pts_np[:, i] = (model_pts_np[:, i]) * ScaleFactor[i] + PredCenter[i] # Scale and then move model origin to origin of prediction depth point cloud
+    #     model_pts_orig_np[:, i] = (model_pts_orig_np[:, i]) * ScaleFactor[i]
 
-    model_pts = PointCloud()
-    model_pts.points = Vector3dVector(model_pts_np)
-    model_pts_orig = PointCloud()
-    model_pts_orig.points = Vector3dVector(model_pts_orig_np)
-    # model_pca = PCA(model_pts_np)
-    # print('Model mean (after transform):', model_pca.mu)
-    # print('Model sigma (after transform):', model_pca.sigma)
+    # model_pts = PointCloud()
+    # model_pts.points = Vector3dVector(model_pts_np)
+    # model_pts_orig = PointCloud()
+    # model_pts_orig.points = Vector3dVector(model_pts_orig_np)
+    # # model_pca = PCA(model_pts_np)
+    # # print('Model mean (after transform):', model_pca.mu)
+    # # print('Model sigma (after transform):', model_pca.sigma)
 
-    trans_init = np.identity(4)
-    print('Apply point-to-point ICP')
-    reg_p2p = registration_icp(model_pts, pred_pts, threshold, trans_init,
-            TransformationEstimationPointToPoint())
-    print(reg_p2p)
-    # print('Transformation is:')
-    # print(reg_p2p.transformation) # OUTPUT 2, 3
-    if isViz:
-        draw_registration_result(model_pts, pred_pts, reg_p2p.transformation)
+    # trans_init = np.identity(4)
+    # print('Apply point-to-point ICP')
+    # reg_p2p = registration_icp(model_pts, pred_pts, threshold, trans_init,
+    #         TransformationEstimationPointToPoint())
+    # print(reg_p2p)
+    # # print('Transformation is:')
+    # # print(reg_p2p.transformation) # OUTPUT 2, 3
+    # if isViz:
+    #     draw_registration_result(model_pts, pred_pts, reg_p2p.transformation)
 
-    FinalTrans = reg_p2p.transformation.copy()
-    # print('Before:\n', FinalTrans)
-    FinalTrans[:3, 3] = FinalTrans[:3, 3] + FinalTrans[:3, :3] @ PredCenter
-    # print('After:\n', FinalTrans)
-    Translation = FinalTrans[:3, 3]
-    Rotation = FinalTrans[:3, :3]
-    # print('PredCenter:', PredCenter)
-    # print('ICP Addition:', reg_p2p.transformation[:3, 3])
-    # print('Final Translation:', Translation)
+    # FinalTrans = reg_p2p.transformation.copy()
+    # # print('Before:\n', FinalTrans)
+    # FinalTrans[:3, 3] = FinalTrans[:3, 3] + FinalTrans[:3, :3] @ PredCenter
+    # # print('After:\n', FinalTrans)
+    # Translation = FinalTrans[:3, 3]
+    # Rotation = FinalTrans[:3, :3]
+    # # print('PredCenter:', PredCenter)
+    # # print('ICP Addition:', reg_p2p.transformation[:3, 3])
+    # # print('Final Translation:', Translation)
 
-    if isViz:
-        draw_registration_result(model_pts_orig, pred_pts, FinalTrans)
+    # if isViz:
+    #     draw_registration_result(model_pts_orig, pred_pts, FinalTrans)
 
+    ScaleFactor = np.array([1, 1, 1])
+    Rotation = np.array([[1,0,0], [0,1,0], [0,0,1]])
+    Translation = np.array([0,0,0])
     return ScaleFactor, Rotation, Translation
 
 
